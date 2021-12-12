339b3d673b54e238931c69cac2d35c0912a4891f
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-12-12 14:11:28 +0100

    Google font setup

--- windi.config.js
+++ windi.config.js
@@ -5,4 +5,16 @@ export default defineConfig({
     include: ["**/*.{jsx,tsx,css}"],
     exclude: ["node_modules", ".git", ".next"],
   },
+  theme: {
+    extend: {
+      fontFamily: {
+        mono: ["Space Mono", "monospace", "ui-monospace", "SFMono-Regular"],
+        display: ["Poppins", "Oswald"],
+        body: ["Cabin", "Open Sans"],
+      },
+      colors: {
+        primary: "#1C3386",
+      },
+    },
+  },
 });
