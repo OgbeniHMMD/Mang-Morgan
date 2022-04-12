29c4c9469df99a71781f88af681cfe9985eb18b4
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-04-12 09:33:54 +0100

    Updated TItle

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import "windi.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>Mang Morgan</title>
+      </Head>
+
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
 export default MyApp;
