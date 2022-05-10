f0ae39aed22d08581e72955b784945ea4364ccda
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-05-10 21:36:28 +0100

    UI Tweak

--- components/home/HomeBusinessesSection.tsx
+++ components/home/HomeBusinessesSection.tsx
@@ -5,14 +5,16 @@ export default function HomeBusinessesSection() {
       style={{ backgroundImage: "url(/images/business-bg.webp)" }}
       className="bg-no-repeat bg-fixed min-h-screen text-left"
     >
-      <div className="container mx-auto min-h-screen text-white w-full py-20 px-4 md:pt-48">
-        <h1 className="font-black font-display text-4xl md:text-6xl lg:text-7xl">
-          Our
-          <span className="text-primary"> Businesses</span>
-        </h1>
-        <div className="text-lg py-12">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit tellus
-          ornare magna id nisl, et.
+      <div className="container flex flex-col mx-auto min-h-screen text-white w-full py-20 px-4 gap-8 justify-between md:pt-48">
+        <div className="w-full">
+          <h1 className="font-black font-display text-4xl md:text-6xl lg:text-7xl">
+            Our
+            <span className="text-primary"> Businesses</span>
+          </h1>
+          <div className="text-lg py-12">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit tellus
+            ornare magna id nisl, et.
+          </div>
         </div>
 
         <div className="flex w-full gap-8 overflow-x-auto">
@@ -31,7 +33,7 @@ export default function HomeBusinessesSection() {
                 style={{
                   backgroundImage: "url(/images/services-001.webp)",
                 }}
-                className="bg-no-repeat bg-cover bg-center flex h-64 items-center aspect-video md:w-auto "
+                className="bg-no-repeat bg-cover bg-center flex h-48 items-center aspect-video md:h-64 md:w-auto lg:h-92"
               >
                 <h1 className="font-black font-display -ml-12 text-2xl z-20 whitespace-nowrap md:text-3xl lg:text-4xl">
                   {service}
