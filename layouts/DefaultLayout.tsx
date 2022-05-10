96c4504ec5ceaf8b3320c96f85ab262f22cdd498
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-05-10 21:22:05 +0100

    Added Title To Layout

--- layouts/DefaultLayout.tsx
+++ layouts/DefaultLayout.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Head from "next/head";
 import FooterSection from "../components/_partials/FooterSection";
 import TopNavigationBar from "../components/_partials/TopNavigationBar";
 
-export default function DefaultLayout({ children }: Children) {
+export default function DefaultLayout({ children, title }: Children) {
   return (
     <main className="font-body scroll-smooth">
+      <Head>{title && <title>{title} | Mang Morgan</title>}</Head>
+
       <TopNavigationBar />
       {children}
       <FooterSection />
@@ -13,5 +16,6 @@ export default function DefaultLayout({ children }: Children) {
 }
 
 interface Children {
+  title?: string;
   children: JSX.Element;
 }
