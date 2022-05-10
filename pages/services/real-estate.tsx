ab57f95aac2476b73aebfbf3714466c7077f2976
OgbeniHMMD <olajide.a.hammed@gmail.com>
2022-05-10 21:26:25 +0100

    Services Page

---
+++ pages/services/real-estate.tsx
@@ -0,0 +1,101 @@
+import DefaultLayout from "../../layouts/DefaultLayout";
+import MeetTheTeamSection from "../../components/about/MeetTheTeamSection";
+import MissionAndVisionSection from "../../components/about/MissionAndVisionSection";
+
+export default function RealEstatePage() {
+  return (
+    <DefaultLayout title="Real Estates">
+      <main className="snap-start">
+        <header
+          style={{ backgroundImage: "url(/images/about/about-001.webp)" }}
+          className="bg-no-repeat bg-bottom bg-fixed"
+        >
+          <div className="bg-black/30 text-white w-full">
+            <div className="container mx-auto py-20 px-4">
+              <h1 className="flex font-black font-display pt-20 text-4xl gap-4 md:text-6xl lg:text-7xl ">
+                <div>Real</div>
+                <div className="text-primary"> Estates</div>
+              </h1>
+              <div className="max-w-screen-sm text-lg py-12 ">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim
+                massa felis nisi magna ut. Mi tortor malesuada proin enim
+                placerat eu. Proin tellus, quis ac leo. Morbi ipsum maecenas id
+                lectus tellus malesuada elit sollicitudin. A dignissim enim,
+                venenatis vitae, a. Odio quisque ac sed pellentesque nibh.
+                Commodo accumsan blandit ut pharetra, vitae lobortis eget non.
+                Velit viverra at nisl felis id dolor in malesuada.
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <section className="bg-white py-20">
+          <div className="container mx-auto px-4">
+            <div className="max-w-screen-md py-12">
+              <h1 className="pb-4 text-2xl md:text-4xl">
+                Morbi ipsum maecenas
+              </h1>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim
+              massa felis nisi magna ut. Mi tortor malesuada proin enim placerat
+              eu. Proin tellus, quis ac leo. Morbi ipsum maecenas id lectus
+              tellus malesuada elit sollicitudin. A dignissim enim, venenatis
+              vitae, a. Odio quisque ac sed pellentesque nibh. Commodo accumsan
+              blandit ut pharetra, vitae lobortis eget non. Velit viverra at
+              nisl felis id dolor in malesuada.
+            </div>
+            <div className="max-w-screen-md py-12 md:ml-auto md:text-right">
+              <h1 className="pb-4 text-2xl md:text-4xl">Vitae lobortis eget</h1>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim
+              massa felis nisi magna ut. Mi tortor malesuada proin enim placerat
+              eu. Proin tellus, quis ac leo. Morbi ipsum maecenas id lectus
+              tellus malesuada elit sollicitudin. A dignissim enim, venenatis
+              vitae, a. Odio quisque ac sed pellentesque nibh. Commodo accumsan
+              blandit ut pharetra, vitae lobortis eget non. Velit viverra at
+              nisl felis id dolor in malesuada.
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-gray-100 py-20">
+          <div className="container mx-auto px-4">
+            <div className="max-w-screen-md py-12">
+              <h1 className="pb-4 text-2xl md:text-4xl">
+                Morbi ipsum maecenas
+              </h1>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim
+              massa felis nisi magna ut. Mi tortor malesuada proin enim placerat
+              eu. Proin tellus, quis ac leo. Morbi ipsum maecenas id lectus
+              tellus malesuada elit sollicitudin. A dignissim enim, venenatis
+              vitae, a. Odio quisque ac sed pellentesque nibh. Commodo accumsan
+              blandit ut pharetra, vitae lobortis eget non. Velit viverra at
+              nisl felis id dolor in malesuada.
+            </div>
+            <div className="max-w-screen-md py-12 md:ml-auto md:text-right">
+              <h1 className="pb-4 text-2xl md:text-4xl">Vitae lobortis eget</h1>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim
+              massa felis nisi magna ut. Mi tortor malesuada proin enim placerat
+              eu. Proin tellus, quis ac leo. Morbi ipsum maecenas id lectus
+              tellus malesuada elit sollicitudin. A dignissim enim, venenatis
+              vitae, a. Odio quisque ac sed pellentesque nibh. Commodo accumsan
+              blandit ut pharetra, vitae lobortis eget non. Velit viverra at
+              nisl felis id dolor in malesuada.
+            </div>
+          </div>
+        </section>
+
+        <section className="bg-white py-20">
+          <div className="container mx-auto max-w-screen-md text-center px-4">
+            <h1 className="pb-4 text-2xl md:text-4xl">Morbi ipsum maecenas</h1>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Enim massa
+            felis nisi magna ut. Mi tortor malesuada proin enim placerat eu.
+            Proin tellus, quis ac leo. Morbi ipsum maecenas id lectus tellus
+            malesuada elit sollicitudin. A dignissim enim, venenatis vitae, a.
+            Odio quisque ac sed pellentesque nibh. Commodo accumsan blandit ut
+            pharetra, vitae lobortis eget non. Velit viverra at nisl felis id
+            dolor in malesuada.
+          </div>
+        </section>
+      </main>
+    </DefaultLayout>
+  );
+}
